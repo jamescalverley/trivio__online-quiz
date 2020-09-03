1763ab953486b8a4a54279f392bca0d43e6d899c
James Calverley <james.calverley3@gmail.com>
2020-09-02 21:20:21 -0400

    adds api routes for quizdata and highscores data

--- server/server.js
+++ server/server.js
@@ -11,7 +11,23 @@ app.use( express.urlencoded({ extended: true }) );
 app.get('/api/test', (req,res) => {
     console.log("REQ --", req.url, req.method);
     res.send("test")
-})
+});
+
+app.get('/api/quiz', (req,res) => {
+    console.log("REQ --", req.url, req.method);
+    const quizData = fs.readFileSync('./data/quizData.json', 'utf-8');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(quizData);
+    console.log("SENDING >>", JSON.parse(quizData));
+});
+
+app.get('/api/scoreboard', (req,res) => {
+    console.log("REQ --", req.url, req.method);
+    const highscores = fs.readFileSync('./data/highscores.json', 'utf-8');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(highscores);
+    console.log("SENDING >> ", JSON.parse(highscores))
+});
 
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`)
