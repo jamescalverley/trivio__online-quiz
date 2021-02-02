e89005834a2b1f825358f2b9e181370d6bc25c81
James Calverley <james.calverley3@gmail.com>
2021-02-01 23:11:13 -0500

    adds complete data set for all 6 quizzes

--- server.js
+++ server.js
@@ -18,6 +18,34 @@ app.use('/', express.static('build'))
 const quizApi = require('./backend/routes/apiRoutes')
 app.use('/api', quizApi )
 
+function checkQuizData( data ){
+    console.log("checking quiz data...");
+    data.forEach( set => {
+        console.log("---> ", set.quizTitle);
+        set.questionSet.forEach( q => {
+            if (q.answers.length === 0 ){
+                console.log(`${q.questionNumber} -- Incomplete`)
+            }
+            else if (q.answers.length === 4 ){
+                const match =  q.answers.find( element => element === q.correctAnswer )
+                
+                if ( match ) {
+                    console.log(`${q.questionNumber} -- CORRECT - MATCH`.green)
+                } else {
+                    console.log(`${q.questionNumber} -- CORRECT - NO MATCH`.red)
+                }
+                   
+            } else {
+                console.log(`${q.questionNumber} -- INCORRECT`.red)
+            }
+        })
+    })
+};
+
+const quizData = require('./backend/db/data/quizData-complete.json');
+
+checkQuizData( quizData );
+
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`.cyan)
 });
