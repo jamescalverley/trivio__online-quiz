368fc99cd91ac1859155e6a57f98b7d300e7bc3e
James Calverley <james.calverley3@gmail.com>
2021-02-08 15:47:09 -0500

    updates server.js to serve public folder through express.static, adds catch all

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 // eslint-disable-next-line no-unused-vars
 const colors = require('colors');
 const dbConnection = require('./backend/db/config/db.js');
@@ -13,10 +14,15 @@ app.use( express.urlencoded({ extended: true }) );
 // mongoDB connection
 dbConnection();
 
-app.use('/*', express.static('build'))
+const quizApi = require('./backend/routes/apiRoutes');
+app.use('/api', quizApi );
 
-const quizApi = require('./backend/routes/apiRoutes')
-app.use('/api', quizApi )
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req,res) => {
+    console.log("----Catch all----", req.url);
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`.cyan)
