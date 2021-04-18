56ab1a0e4223188027d6d18308e4336d58e99e67
James Calverley <james.calverley3@gmail.com>
2021-04-18 14:16:42 -0400

    sets up config file

--- backend/db/config/db.js
+++ backend/db/config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const uri = process.env.MONGO_URI;
+//const uri = process.env.MONGO_URI;
+
+const { mongoURI } = require('../../../config.js')
 
 const dbConnection = async () => {
   try {
-    const connect = await mongoose.connect( uri, { 
+    const connect = await mongoose.connect( mongoURI, { 
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true
